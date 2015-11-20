c4527e7e487a3e39769522e156e1260765ecdf86
Joe Doss <jdoss@kennasecurity.com>
2015-11-20 00:54:33 -0600

    Adjust formatting

--- lib/fluent/plugin/parser_es_slow_indexing.rb
+++ lib/fluent/plugin/parser_es_slow_indexing.rb
@@ -1,58 +1,60 @@
 module Fluent
-  class ElasticsearchSlowIndexingLogParser < Parser
-    REGEXP = /^\[(?<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\]\[(?<severity>[a-zA-Z\s]+)\]\[(?<source>\S+)\] \[(?<node>\S+)\] \[(?<index>\w+)\]\[(?<shard>\d+)\] took\[(?<took>.+)\], took_millis\[(?<took_millis>\d+)\], type\[(?<type>.+)\], id\[(?<indexing_id>.*)\], routing\[(?<routing>.*)\], source\[(?<source_body>.*)\]/
-    TIME_FORMAT = "%Y-%m-%d %H:%M:%S,%N"
+  class TextParser
+    class ElasticsearchSlowIndexingLogParser < Parser
+      REGEXP = /^\[(?<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\]\[(?<severity>[a-zA-Z\s]+)\]\[(?<source>\S+)\] \[(?<node>\S+)\] \[(?<index>\w+)\]\[(?<shard>\d+)\] took\[(?<took>.+)\], took_millis\[(?<took_millis>\d+)\], type\[(?<type>.+)\], id\[(?<indexing_id>.*)\], routing\[(?<routing>.*)\], source\[(?<source_body>.*)\]/
+      TIME_FORMAT = "%Y-%m-%d %H:%M:%S,%N"
 
-    Plugin.register_parser("es_slow_indexing", self)
+      Plugin.register_parser("es_slow_indexing", self)
 
-    def initialize
-      super
-      @time_parser = TextParser::TimeParser.new(TIME_FORMAT)
-      @mutex = Mutex.new
-    end
-
-    def patterns
-      {'format' => REGEXP, 'time_format' => TIME_FORMAT}
-    end
-
-    def parse(text)
-      m = REGEXP.match(text)
-      unless m
-        if block_given?
-          yield nil, nil
-          return
-        else
-          return nil, nil
-        end
+      def initialize
+        super
+        @time_parser = TextParser::TimeParser.new(TIME_FORMAT)
+        @mutex = Mutex.new
       end
 
-      shard = m['shard'].to_i
-      took_millis = m['took_millis'].to_i
-      indexing_id= m['indexing_id'].to_i
-      routing = m['routing'].to_i
+      def patterns
+        {'format' => REGEXP, 'time_format' => TIME_FORMAT}
+      end
 
-      time = m['time']
-      time = @mutex.synchronize { @time_parser.parse(time) }
+      def parse(text)
+        m = REGEXP.match(text)
+        unless m
+          if block_given?
+            yield nil, nil
+            return
+          else
+            return nil, nil
+          end
+        end
 
-      record = {
-        'severity' => m['severity'],
-        'source' => m['source'],
-        'node' => m['node'],
-        'index' => m['index'],
-        'shard' => shard,
-        'took' => m['took'],
-        'took_millis' => took_millis,
-        'type' => m['type'],
-        'indexing_id' => indexing_id,
-        'routing' => routing,
-        'source_body' => m['source_body']
-      }
-      record["time"] = m['time'] if @keep_time_key
+        shard = m['shard'].to_i
+        took_millis = m['took_millis'].to_i
+        indexing_id= m['indexing_id'].to_i
+        routing = m['routing'].to_i
 
-      if block_given?
-        yield time, record
-      else
-        return time, record
+        time = m['time']
+        time = @mutex.synchronize { @time_parser.parse(time) }
+
+        record = {
+          'severity' => m['severity'],
+          'source' => m['source'],
+          'node' => m['node'],
+          'index' => m['index'],
+          'shard' => shard,
+          'took' => m['took'],
+          'took_millis' => took_millis,
+          'type' => m['type'],
+          'indexing_id' => indexing_id,
+          'routing' => routing,
+          'source_body' => m['source_body']
+        }
+        record["time"] = m['time'] if @keep_time_key
+
+        if block_given?
+          yield time, record
+        else
+          return time, record
+        end
       end
     end
   end
--- lib/fluent/plugin/parser_es_slow_query.rb
+++ lib/fluent/plugin/parser_es_slow_query.rb
@@ -3,57 +3,58 @@ module Fluent
     REGEXP = /^\[(?<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\]\[(?<severity>[a-zA-Z]+\s*)\]\[(?<source>\S+)\] \[(?<node>\S+)\] \[(?<index>\w+)\]\[(?<shard>\d+)\] took\[(?<took>.+)\], took_millis\[(?<took_millis>\d+)\], types\[(?<types>.*)\], stats\[(?<stats>.*)\], search_type\[(?<search_type>.*)\], total_shards\[(?<total_shards>\d+)\], source\[(?<source_body>.*)\], extra_source\[(?<extra_source>.*)\]/
     TIME_FORMAT = "%Y-%m-%d %H:%M:%S,%N"
 
-    Plugin.register_parser("es_slow_query", self)
+      Plugin.register_parser("es_slow_query", self)
 
-    def initialize
-      super
-      @time_parser = TextParser::TimeParser.new(TIME_FORMAT)
-      @mutex = Mutex.new
-    end
-
-    def patterns
-      {'format' => REGEXP, 'time_format' => TIME_FORMAT}
-    end
-
-    def parse(text)
-      m = REGEXP.match(text)
-      unless m
-        if block_given?
-          yield nil, nil
-          return
-        else
-          return nil, nil
-        end
+      def initialize
+        super
+        @time_parser = TextParser::TimeParser.new(TIME_FORMAT)
+        @mutex = Mutex.new
       end
 
-      shard = m['shard'].to_i
-      took_millis = m['took_millis'].to_i
-      total_shards = m['total_shards'].to_i
+      def patterns
+        {'format' => REGEXP, 'time_format' => TIME_FORMAT}
+      end
 
-      time = m['time']
-      time = @mutex.synchronize { @time_parser.parse(time) }
+      def parse(text)
+        m = REGEXP.match(text)
+        unless m
+          if block_given?
+            yield nil, nil
+            return
+          else
+            return nil, nil
+          end
+        end
 
-      record = {
-        'severity' => m['severity'],
-        'source' => m['source'],
-        'node' => m['node'],
-        'index' => m['index'],
-        'shard' => shard,
-        'took' => m['took'],
-        'took_millis' => took_millis,
-        'types' => m['types'],
-        'stats' => m['stats'],
-        'search_type' => m['search_type'],
-        'total_shards' => total_shards,
-        'source_body' => m['source_body'],
-        'extra_source' => m['extra_source']
-      }
-      record["time"] = m['time'] if @keep_time_key
+        shard = m['shard'].to_i
+        took_millis = m['took_millis'].to_i
+        total_shards = m['total_shards'].to_i
 
-      if block_given?
-        yield time, record
-      else
-        return time, record
+        time = m['time']
+        time = @mutex.synchronize { @time_parser.parse(time) }
+
+        record = {
+          'severity' => m['severity'],
+          'source' => m['source'],
+          'node' => m['node'],
+          'index' => m['index'],
+          'shard' => shard,
+          'took' => m['took'],
+          'took_millis' => took_millis,
+          'types' => m['types'],
+          'stats' => m['stats'],
+          'search_type' => m['search_type'],
+          'total_shards' => total_shards,
+          'source_body' => m['source_body'],
+          'extra_source' => m['extra_source']
+        }
+        record["time"] = m['time'] if @keep_time_key
+
+        if block_given?
+          yield time, record
+        else
+          return time, record
+        end
       end
     end
   end
